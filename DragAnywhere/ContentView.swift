2e1cfad3fc1c2610402196dce363e632e15f60e9
Adam C <windexplorer@outlook.com>
2023-10-27 21:46:42 +0800

    Initial Commit

---
+++ DragAnywhere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DragAnywhere
+//
+//  Created by Adam Chen JingFan on 2023/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DragAnywhere/DragAnywhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAnywhereApp.swift
+//  DragAnywhere
+//
+//  Created by Adam Chen JingFan on 2023/10/27.
+//
+
+import SwiftUI
+
+@main
+struct DragAnywhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
